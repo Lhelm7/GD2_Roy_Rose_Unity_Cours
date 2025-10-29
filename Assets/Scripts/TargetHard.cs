05b7f78f0924590bc9971c4f39ad02d311c72e51
Lhelm7 <roroleroy84@gmail.com>
2025-10-29 15:30:45 +0100

    mise en place level 2 / mise en place sound design son sur le collectible energie

--- Assets/Scripts/TargetHard.cs
+++ Assets/Scripts/TargetHard.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class TargetHard : MonoBehaviour
 {
     [SerializeField] private int _targetValue = 1;
-    private CollectibleRespawn _respawn;
-
+    private CollectibleRespawn _respawn; 
+    [SerializeField] private AudioClip _audioSource;
     private void Awake()
     {
         _respawn = GetComponent<CollectibleRespawn>();
@@ -16,12 +16,16 @@ public class TargetHard : MonoBehaviour
         if (other.gameObject.GetComponent<PlayerCollect>() != null)
         {
             other.gameObject.GetComponent<PlayerCollect>().UpdateScore(_targetValue);
-
+            
+            if (_audioSource != null)
+                AudioSource.PlayClipAtPoint(_audioSource, transform.position);
+            
             if (_respawn != null)
             {
                 _respawn.Collect();
             }
             else
+            
             {
                 Destroy(gameObject);
             }
