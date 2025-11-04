a8f4e7e63108d474ac701596cb9b047c20781bc3
Lhelm7 <roroleroy84@gmail.com>
2025-11-04 02:09:40 +0100

    [RRO] Premier build positif pas de problème / et légère retouche UI et music mais projet fini (enfin reste beaucoup d'ameliorations et de logique à revoir mais le jeu est la et est jouable)

--- Assets/Menu/Scripts/MainMenu.cs
+++ Assets/Menu/Scripts/MainMenu.cs
@@ -5,18 +5,20 @@ public class MainMenu : MonoBehaviour
 {
     private string _Level1Scene = "Dev_Map";
     [SerializeField] private AudioSource music;
-
+    private static bool musicHasPlayed = false;
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
     public void Play()
         {
-            if (music != null && !music.isPlaying)
+            if (music != null && !musicHasPlayed)
+            {
                 music.Play();
+                musicHasPlayed = true;
+            }
             SceneManager.LoadScene(_Level1Scene);
         }
-
         public void Quit()
         {
             Application.Quit();
